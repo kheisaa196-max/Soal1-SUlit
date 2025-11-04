50a141fb6628e185aa37d8d3abdb28634ba010af
kheishaa <kheisaa196@gmail.com>
2025-11-04 16:28:12 +0700

    Add files via upload

---
+++ Nilai rata-rata.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class NilaiRataRata {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = input.nextInt();
+
+        double totalNilai = 0;
+
+        for (int i = 1; i <= jumlahSiswa; i++) {
+            System.out.print("Masukkan nilai siswa ke-" + i + ": ");
+            double nilai = input.nextDouble();
+            totalNilai += nilai;
+        }
+
+        double rataRata = totalNilai / jumlahSiswa;
+
+        System.out.println("\n===== HASIL REKAP NILAI =====");
+        System.out.println("Jumlah siswa: " + jumlahSiswa);
+        System.out.println("Total nilai: " + totalNilai);
+        System.out.println("Nilai rata-rata: " + rataRata);
+
+        input.close();
+    }
+}
